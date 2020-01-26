fea534713bbcc187dd5bb6c36868d79328015806
geofis <joseramon@geografiafisica.org>
2020-01-25 21:42:08 -0400

    added clumps counts

--- estadistica_zonal_objetos_contar_cumulos.R
+++ estadistica_zonal_objetos_contar_cumulos.R
@@ -7,11 +7,13 @@ ezonalclumps <- function(objraster = NULL, nombre = '', objgeometrias = NULL, ex
   names(r) <- nombre
   geoms <- objgeometrias
   df <- raster::extract(x = r, y = as(geoms, 'Spatial'), df = T)
-  dfresumen <- t(sapply(unique(df[,'ID']), function(x) 
-    n = length(na.omit(df[df[,'ID']==x,nombre], na.rm = T))))
-  colnames(dfresumen) <- paste0(nombre, '_', colnames(dfresumen))
-  colnames(dfresumen) <- gsub('\\.', '', colnames(dfresumen))
-  # return(dfresumen)
+  dfresumen <- t(sapply(unique(df[,'ID']), function(x) {
+    d <- data.frame(
+      ID = x,
+      n = length(na.omit(df[df[,'ID']==x, nombre], na.rm = T)))
+    }))
+  dfresumen <- dfresumen[,'n', drop = F]
+  colnames(dfresumen) <- nombre
   geomsout <- dplyr::bind_cols(geoms, as.data.frame(dfresumen))
   if (export) {
     st_write(geomsout, dsn = paste0('salidas_ezonal/', nombreexport, '.gpkg'), driver = 'GPKG')
